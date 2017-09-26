d1f79b665eadf1795994d7eccffd7c6f08df4ea6
Joe Sogard <jmsogard@gmail.com>
2017-09-25 22:04:45 -0500

    forgot one

--- utils.h
+++ utils.h
@@ -7,6 +7,7 @@
 
 // parses `entire_line` into `words` delimited by " " and returns highest index + 1
 int parse_words(char *entire_line, char *words[]);
+void next_command(char *user_command);
 int execute_command(char *args[]);
 int check_concurrency(char *args[]);
 void remove_trailing_n(char *command);
